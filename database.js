0ec0d7104402a3fdffd76a7b0c7a84bb045be1ad
Scvrlae <yurmepsycliv@gmail.com>
2025-09-10 21:18:06 +0200

    Chore: Create database.js

---
+++ database.js
@@ -0,0 +1,17 @@
+/*
+
+	This code was written by Scvrlae (@datonescvrlae) and is his property. Unauthorized use and/or
+	distribution of this script is strongly prohibited! PLEASE ask first!
+
+	  ██████  ▄████▄ ██▒   █▓ ██▀███   ██▓    ▄▄▄      ▓█████ 
+	▒██    ▒ ▒██▀ ▀█▓██░   █▒▓██ ▒ ██▒▓██▒   ▒████▄    ▓█   ▀ 
+	░ ▓██▄   ▒▓█    ▄▓██  █▒░▓██ ░▄█ ▒▒██░   ▒██  ▀█▄  ▒███   
+	  ▒   ██▒▒▓▓▄ ▄██▒▒██ █░░▒██▀▀█▄  ▒██░   ░██▄▄▄▄██ ▒▓█  ▄ 
+	▒██████▒▒▒ ▓███▀ ░ ▒▀█░  ░██▓ ▒██▒░██████▒▓█   ▓██▒░▒████▒
+	
+	This file is part of the Amniva Manager Discord bot owned and maintained by Scvrlae (@datonescvrlae).
+	It is a public resource that can be used for learning!
+
+*/
+
+const MySQL = require("mysql") // Module used to communicate with SQL databases
